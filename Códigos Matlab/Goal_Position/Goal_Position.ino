978aeb8358b84cb65a1a1296351ab1425592eaa1
SamuelCruz72 <sacruzs@unal.edu.co>
2025-07-25 14:35:01 -0500

    update matlab

--- Códigos Matlab/Goal_Position/Goal_Position.ino
+++ Códigos Matlab/Goal_Position/Goal_Position.ino
@@ -1,11 +1,11 @@
 #include <Dynamixel2Arduino.h>
 
 #define DXL_SERIAL Serial1
+#define DEBUG_SERIAL Serial
+
 const uint8_t DXL_ID = 1;
 const float DXL_PROTOCOL_VERSION = 2.0;
 const uint32_t DXL_BAUDRATE = 57600;
-const uint32_t PROFILE_VELOCITY = 50;
-const uint32_t PROFILE_ACCELERATION = 10;
 
 Dynamixel2Arduino dxl(DXL_SERIAL);
 
@@ -20,8 +20,8 @@ void setup() {
   dxl.torqueOff(DXL_ID);
   dxl.setOperatingMode(DXL_ID, OP_POSITION);
 
-  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 50);      // Velocidad Ajustable
-  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 10);  // Arranque Suave
+  dxl.writeControlTableItem(ControlTableItem::PROFILE_VELOCITY, DXL_ID, 100);      // Velocidad Ajustable
+  dxl.writeControlTableItem(ControlTableItem::PROFILE_ACCELERATION, DXL_ID, 30);  // Arranque Suave
 
   dxl.torqueOn(DXL_ID);
 
@@ -34,8 +34,7 @@ void loop() {
     if (input.startsWith("POS_GRADOS:")) {
       float grados = input.substring(11).toFloat();
       grados = constrain(grados, 0.0, 180.0);
-      uint16_t pos_dxl = static_cast<uint16_t>((270 - grados) / 180.0 * 4096);
-      dxl.setGoalPosition(DXL_ID, pos_dxl);
+      dxl.setGoalPosition(DXL_ID, grados, UNIT_DEGREE);
 
       Serial.print("POS_GRADOS:");
       Serial.println(grados);
--- Códigos Matlab/Goal_Position/Goal_Position.m
+++ Códigos Matlab/Goal_Position/Goal_Position.m
@@ -3,7 +3,7 @@ configureTerminator(s, "LF");
 pause(2);  
 
 pause(0.5);  
-for ang = 0:30:180
+for ang = 180:30:360
     cmd = sprintf("POS_GRADOS:%.2f", ang);
     writeline(s, cmd);
     fprintf("Enviado: %.f\n", ang);
